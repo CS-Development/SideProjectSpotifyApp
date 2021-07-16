0f8f8d5d361c4f49b850cb96eb486789169ba958
Christian Slanzi <christian.slanzi@gmail.com>
2021-07-16 14:45:52 +0200

    created App's Xcode Project

---
+++ Sources/App/SideProjectSpotifyApp/SideProjectSpotifyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideProjectSpotifyApp
+//
+//  Created by Christian Slanzi on 16.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
