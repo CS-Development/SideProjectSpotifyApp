83c2b0198b950b9088aa4e0a8836aed0f77df323
Christian Slanzi <christian.slanzi@gmail.com>
2021-07-19 22:00:39 +0200

    usage of SpotifyApiRemote

--- Sources/App/SideProjectSpotifyApp/SideProjectSpotifyApp/ViewController.swift
+++ Sources/App/SideProjectSpotifyApp/SideProjectSpotifyApp/ViewController.swift
@@ -7,115 +7,34 @@
 
 import UIKit
 import NetworkingService
+import SpotifyApiModule
 
 class ViewController: UIViewController {
 
+    var client: URLSessionHTTPClient!
+    var service: SpotifyApiRemote!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         view.backgroundColor = .systemPink
         
-        standardGetUrlRequestWithJsonAnswer()
+        client = URLSessionHTTPClient(session: URLSession.shared)
+        service = SpotifyApiRemote(url: URL(string: "https://accounts.spotify.com/api")!, client: client, accessToken: AccessTokenDTO(accessToken: "BQD8NtKU2MjsTOqup1RyScIvlmZoP_x53ZGabiRSHBGqvPIkRxE7dIU8IbUhsmeZPUkec8boXLr_gst5L-o", tokenType: "Bearer", expiresIn: 3600))
+        
+        refactoredUrlRequest()
     }
     
     func refactoredUrlRequest() {
-        
-        let client = URLSessionHTTPClient(session: URLSession.shared)
-        client.requestHttpHeaders.add(value: "Bearer BQD8NtKU2MjsTOqup1RyScIvlmZoP_x53ZGabiRSHBGqvPIkRxE7dIU8IbUhsmeZPUkec8boXLr_gst5L-o", forKey: "Authorization")
-        client.makeRequest(toURL: URL(string:"https://api.spotify.com/v1/getNewReleases")!, withHttpMethod: .get) { result in
-            //
-            let album:AlbumModelDTO = GenericDecoder.decodeResult(result: result)
-        }
-    }
-    
-//    curl -X "POST" -H "Authorization: Basic ZDY3MGE1OTlmZGM2NGI1ZjkyMGY4MmI0NDUwYTlhNGU6NThkZDE3NmQ4YmJhNGE3YjkzNTA4NGMwNmU3ZDNlYTE=" -d grant_type=client_credentials https://accounts.spotify.com/api/token
-
-    func standardGetUrlRequestWithJsonAnswer() {
-        
-        
-        // Create URL
-        let url = URL(string: "https://api.spotify.com/v1/getNewReleases")
-        guard let requestUrl = url else { fatalError() }
-        // Create URL Request
-        var request = URLRequest(url: requestUrl)
-        // Specify HTTP Method to use
-        request.httpMethod = "GET"
-        
-        
-        
-        
-        // Set HTTP Request Header
-        request.setValue("Bearer BQD8NtKU2MjsTOqup1RyScIvlmZoP_x53ZGabiRSHBGqvPIkRxE7dIU8IbUhsmeZPUkec8boXLr_gst5L-o", forHTTPHeaderField: "Authorization")
-        
-        
-        
-        
-        
-        // Send HTTP Request
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            
-            // Check if Error took place
-            if let error = error {
-                print("Error took place \(error)")
-                return
+        service.getAllNewReleases { result in
+            switch result {
+            case let .failure(error):
+                print(error)
+            case let .success(dto):
+                print(dto)
             }
-            
-            // Read HTTP Response Status code
-            if let response = response as? HTTPURLResponse {
-                print("Response HTTP Status code: \(response.statusCode)")
-            }
-            
-            // Convert HTTP Response Data to a simple String
-            if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                print("Response data string:\n \(dataString)")
-                
-                let album = self.parseJSON(data: data)
-                print(album)
-            }
-        
         }
-        task.resume()
-    }
-    
-    func parseJSON(data: Data) -> AlbumModelDTO? {
-        
-        var returnValue: AlbumModelDTO?
-        do {
-            returnValue = try JSONDecoder().decode(AlbumModelDTO.self, from: data)
-        } catch {
-            print("Error took place\(error.localizedDescription).")
-        }
-        
-        return returnValue
-    }
-
-}
-
-public struct AlbumModelDTO: DTO {
-    
-    public var href: String
-    public var name: String
-    public var numOfSongs: Int
-    
-    //
-    
-    //
-    
-    //
-    
-    //
-    
-    //
-
-    public var description: String {
-        return """
-        ------------
-        href = \(href)
-        name = \(name)
-        numOfSongs = \(numOfSongs)
-        ------------
-        """
     }
 }
 
