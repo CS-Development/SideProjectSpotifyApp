d3b4966915ab4ffc4dba8ee31eecac58273eb2ad
Christian Slanzi <christian.slanzi@gmail.com>
2021-07-28 19:13:47 +0200

    add SpotifyAccountApiModule framework project

---
+++ Sources/Modules/SpotifyAccountApiModule/SpotifyAccountApiModule/SpotifyAccountApiModule.h
@@ -0,0 +1,18 @@
+//
+//  SpotifyAccountApiModule.h
+//  SpotifyAccountApiModule
+//
+//  Created by Christian Slanzi on 28.07.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpotifyAccountApiModule.
+FOUNDATION_EXPORT double SpotifyAccountApiModuleVersionNumber;
+
+//! Project version string for SpotifyAccountApiModule.
+FOUNDATION_EXPORT const unsigned char SpotifyAccountApiModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpotifyAccountApiModule/PublicHeader.h>
+
+
