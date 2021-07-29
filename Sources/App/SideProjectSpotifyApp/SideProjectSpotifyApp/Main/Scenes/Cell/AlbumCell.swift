624fcfe0f55ca3cd29884da1bd6fe65a826c43d2
Pawel Kacela <you@example.com>
2021-07-29 21:03:34 +0200

    add AlbumCell view

--- Sources/App/SideProjectSpotifyApp/SideProjectSpotifyApp/Main/Scenes/Cell/AlbumCell.swift
+++ Sources/App/SideProjectSpotifyApp/SideProjectSpotifyApp/Main/Scenes/Cell/AlbumCell.swift
@@ -9,13 +9,74 @@ import UIKit
 
 class AlbumCell: UICollectionViewCell {
     
+    let cornerRadiusValue: CGFloat = 10
+    
+   private let wrapperView: UIView = {
+       let view = UIView()
+        view.backgroundColor = .white
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "This is test album name"
+        label.font = UIFont.systemFont(ofSize: 24, weight: .regular)
+        return label
+    }()
+    
+    private let albumImage: UIImageView = {
+        let imager = UIImageView()
+        imager.contentMode = UIView.ContentMode.scaleAspectFit
+        return imager
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
+        setupHierarchy()
+        setupLayout()
     }
     
     private func setupViews(){
-        backgroundColor = .blue
+        backgroundColor = .white
+        layer.cornerRadius = cornerRadiusValue
+        wrapperView.layer.cornerRadius = cornerRadiusValue
+        layer.addShadow()
+        [wrapperView, titleLabel, albumImage].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    
+    private func setupHierarchy() {
+        addSubview(wrapperView)
+        [titleLabel, albumImage].forEach {
+            wrapperView.addSubview($0)
+        }
+    }
+    
+    private func setupLayout() {
+        
+        NSLayoutConstraint.activate([
+            wrapperView.leftAnchor.constraint(equalTo: self.leftAnchor),
+            wrapperView.topAnchor.constraint(equalTo: self.topAnchor),
+            wrapperView.rightAnchor.constraint(equalTo: self.rightAnchor),
+            wrapperView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            albumImage.leftAnchor.constraint(equalTo: wrapperView.leftAnchor),
+            albumImage.topAnchor.constraint(equalTo: wrapperView.topAnchor),
+            albumImage.rightAnchor.constraint(equalTo: wrapperView.rightAnchor),
+            albumImage.heightAnchor.constraint(equalToConstant: 140)
+        ])
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: albumImage.bottomAnchor),
+            titleLabel.leftAnchor.constraint(equalTo: wrapperView.leftAnchor, constant: 16),
+            titleLabel.rightAnchor.constraint(equalTo: wrapperView.rightAnchor, constant: -16),
+            titleLabel.bottomAnchor.constraint(equalTo: wrapperView.bottomAnchor)
+        ])
     }
     
     class var identifier: String {
---
+++ Sources/App/SideProjectSpotifyApp/SideProjectSpotifyApp/Main/Scenes/Cell/AlbumCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AlbumCellViewModel.swift
+//  SideProjectSpotifyApp
+//
+//  Created by Pawel Kacela on 29/07/2021.
+//
+
+import Foundation
