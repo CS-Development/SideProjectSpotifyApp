5d84beb1e5fd8606ec815625362df33e93648c2b
Pawel Kacela <you@example.com>
2021-09-17 21:05:36 +0200

    improved album detail view layout, add date formatting

--- Sources/App/SideProjectSpotifyApp/SideProjectSpotifyApp/Main/Scenes/Albums/AlbumDetailViewController.swift
+++ Sources/App/SideProjectSpotifyApp/SideProjectSpotifyApp/Main/Scenes/Albums/AlbumDetailViewController.swift
@@ -24,14 +24,15 @@ class AlbumDetailViewController: UIViewController {
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.text = "This is test album name"
-        label.font = UIFont.systemFont(ofSize: 36, weight: .regular)
+        label.font = UIFont.systemFont(ofSize: 32, weight: .regular)
+        label.numberOfLines = 2
         return label
     }()
     
     private let releaseDateLabel: UILabel = {
         let label = UILabel()
         label.text = "This is test album releaseDate"
-        label.font = UIFont.systemFont(ofSize: 26, weight: .regular)
+        label.font = UIFont.systemFont(ofSize: 24, weight: .regular)
         return label
     }()
     
@@ -61,14 +62,14 @@ class AlbumDetailViewController: UIViewController {
     private func update(viewModel: AlbumDetailViewModel) {
         let album = viewModel.album
         titleLabel.text = album.name
-        releaseDateLabel.text = album.releaseDate
+        releaseDateLabel.text = viewModel.releaseDate
         albumImage.image = UIImage(data: viewModel.imageData)
     }
     
     private func setupViews() {
         view.backgroundColor = .white
         
-        [stackView, titleLabel, albumImage, ].forEach {
+        [stackView, titleLabel, albumImage, releaseDateLabel].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
     }
@@ -85,7 +86,20 @@ class AlbumDetailViewController: UIViewController {
             stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             stackView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
             stackView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
-            albumImage.heightAnchor.constraint(equalToConstant: 376)
+        ])
+        
+        NSLayoutConstraint.activate([
+            albumImage.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 32),
+            albumImage.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -32),
+            albumImage.topAnchor.constraint(equalTo: stackView.topAnchor, constant: 32),
+            albumImage.heightAnchor.constraint(equalToConstant: 300)
+        ])
+        
+        NSLayoutConstraint.activate([
+            titleLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16),
+            titleLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16),
+            releaseDateLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16),
+            releaseDateLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16),
         ])
     }
     
--- Sources/App/SideProjectSpotifyApp/SideProjectSpotifyApp/Main/Scenes/Albums/AlbumDetailViewModel.swift
+++ Sources/App/SideProjectSpotifyApp/SideProjectSpotifyApp/Main/Scenes/Albums/AlbumDetailViewModel.swift
@@ -16,6 +16,19 @@ class AlbumDetailViewModel {
         self.album = album
     }
     
+    var releaseDate: String? {
+        let dateFormatterGet = DateFormatter()
+        dateFormatterGet.dateFormat = "yyyy-MM-dd"
+
+        let dateFormatterPrint = DateFormatter()
+        dateFormatterPrint.dateFormat = "MMM d, yyyy"
+        
+        guard let date = dateFormatterGet.date(from: album.releaseDate) else { return nil }
+        let dateToDisplay = dateFormatterPrint.string(from: date)
+        
+        return "Release Date: \(dateToDisplay)"
+    }
+    
     lazy var imageData: Data = {
         return loadImageData(urlString: album.images[0].url) ?? Data()
     }()
